7835e0e53d40789b96f149af3a0661ecb6befc94
Darwin Ramos <2014110527@ub.edu.bz>
2025-03-22 21:21:40 -0600

    3rd commit, added the controller to add a new task and to catch common errors

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -10,9 +10,20 @@ export const renderHomePage = async (req, res) => {
   }
 };
 
+export const createTask = async(req, res)=>{
+    const {title, description} = req.body;
 
-
-
+    if(!title || title.lenght < 3 || title.lenght > 50){
+        return res.status(400).render("error", {message: "Invalid task title"});
+    }
+    try{
+        await addTask(title, description);
+        res.redirect("/");
+    }catch(error){
+        res.status(500).render("error",{message: "Failed to create task"});
+    }
+};
+  
 
 
 
